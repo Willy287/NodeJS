42476da89f831a0c5df1802143bb31b3e6564474
Wilmer Alberto Gonzalez Machuca <wagonzalez@personalsoft.com.co>
2021-03-09 14:29:52 -0500

    Se sube codigo de ejercicio NodeJS

---
+++ NODEJS/histogram.js
@@ -0,0 +1,59 @@
+class DefaultMap extends Map{
+
+    constructor(defaultValue){
+        super();
+        this.defaultValue = defaultValue;
+    }   
+
+    get(key){
+        if(this.has(key)){
+            return super.get(key);
+        }
+        else{
+            return this.defaultValue;
+        }
+    }
+}
+
+class Histograma{
+    constructor(){
+        this.letterCounts = new DefaultMap(0);
+        this.totalLetters  = 0;
+    }
+
+    add(text){
+        text=text.replace(/\s/g,"").toUpperCase();
+        for(let character of text){
+            let count  = this.letterCounts.get(character);
+            this.letterCounts.set(character,count+1);
+            this.totalLetters++;
+        }
+    }
+    
+    toString(){
+        let entries =[...this.letterCounts];
+        entries.sort((a,b)=> {
+            if(a[1] === b[1]){
+                return a[0]<b[0]? -1 : 1;
+            }
+            return b[1] - a[1];
+        });
+        for(let entri of entries){
+            entri[1] = (entri[1]/this.totalLetters) * 100 ;
+        }
+        entries=entries.filter(entri=>entri[1]>=1);
+        let lines = entries.map( ([l,n]) => `Letra  ${l} : ${"#".repeat(Math.round(n))} ${n.toFixed(2)}%`);
+        return lines.join("\n");
+    }
+}
+
+async function histogramFromStdin(){
+    process.stdin.setEncoding("utf-8");
+    let probar  = new Histograma();
+    for await( let chunk of  process.stdin){
+        probar.add(chunk);
+    }
+    return probar;
+}
+
+histogramFromStdin().then((e) => console.log(e.toString())).catch((e)=>console.log(e));
