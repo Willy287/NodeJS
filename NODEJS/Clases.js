109c1bc48196216c809eb99212779d33d9b67bef
Wilmer Alberto Gonzalez Machuca <wagonzalez@personalsoft.com.co>
2021-03-17 15:24:11 -0500

    Se incluyen temas de modulos, clases y ejercicio propio de clases

---
+++ NODEJS/Clases.js
@@ -0,0 +1,154 @@
+/**
+ * Clases en JavaScript
+ * En JS una clase es un conjunto de objetos que heredan propiedades del mismo objeto prototipo.
+ * El objeto prototipo es la caracteristica general de una clase.
+ */
+
+//Forma antigua de crear clases en JS.
+//Esto es un constructor que inicializa nuevos objetos.
+function Range(from, to) {
+    this.from = from;
+    this.to = to;
+}
+//Funciona para rangos textuales, de fecha y númerico. Devuelve verdadero si x está en el rango.
+Range.prototype = {
+    includes: function (x) {
+        return this.from <= x && x <= this.to;
+    },
+    [Symbol.iterator]: function* () { //Función generadora que hace iterables los rangos de nuetsra clase.
+        for (let x = Math.ceil(this.from); x <= this.to; x++) {
+            yield x;
+        }
+    },
+    toString: function () {
+        return "(" + this.from + "..." + this.to + ")";
+    }
+};
+
+let range = new Range(1, 10);
+console.log(range.includes(3));
+console.log(range.includes(30));
+console.log(range.toString());
+console.log([...range]);
+
+//Forma actual de crear clases en JS
+
+class RangeCurrent {
+    constructor(from, to) {
+        this.from = from;
+        this.to = to;
+    }
+    includes(x) {
+        return this.from <= x && x <= this.to;
+    }
+    *[Symbol.iterator]() { //Función generadora que hace iterables los rangos de nuetsra clase.
+        for (let x = Math.ceil(this.from); x <= this.to; x++) {
+            yield x;
+        }
+    }
+    toString() {
+        return "(" + this.from + "..." + this.to + ")";
+    }
+}
+let rangeCurrent = new RangeCurrent(1, 10);
+console.log(rangeCurrent.includes(3));
+console.log(rangeCurrent.includes(30));
+console.log(rangeCurrent.toString());
+console.log([...rangeCurrent]);
+
+class Span extends RangeCurrent {
+    constructor(start, length) {
+        if (length >= 0) {
+            super(start, start + length);
+        } else {
+            super(start + length, start);
+        }
+    }
+}
+
+let span1 = new Span(1, 10);
+console.log(span1.includes(5));
+console.log(span1.toString());
+
+//Función como expresión
+let square = function (x) {
+    return x * x;
+}
+console.log(square(6));
+
+let Square = class {
+    constructor(x) {
+        this.area = x * x;
+    }
+}
+
+console.log(new Square(4).area);
+
+let squareObject = new Square(7);
+console.log(squareObject.area);
+/**----------------------------------------------------------------------------------- */
+/**
+ * Ejemplos de clases propios WILMER GONZALEZ
+ * 
+ */
+
+//Forma antigua de crear clases
+function Edad(firstname, lastname, birthday) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.birthday = new Date(birthday);
+}
+
+Edad.prototype = {
+    getEdad: function () {
+        var ageDifMs = Date.now() - this.birthday.getTime();
+        var ageDate = new Date(ageDifMs);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+};
+
+let edad = new Edad("Wilmer", "Gonzalez", "04-07-1992");
+console.log(edad.getEdad());
+
+
+//Forma Actual de crear clases
+class Person {
+    constructor(firstname, lastname, birthday) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthday = new Date(birthday);
+    }
+    getAge() {
+        var ageDifMs = Date.now() - this.birthday.getTime();
+        var ageDate = new Date(ageDifMs);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+}
+
+let person = new Person("Wilmer", "Gonzalez", "04-07-1992");
+console.log(person.getAge());
+
+//Función como expresión
+let age = function (birthday) {
+    let date = new Date(birthday);
+    var ageDifMs = Date.now() - date.getTime();
+    var ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+console.log(age("04-07-1992"));
+
+let ageWilmer = class {
+    constructor(firstname, lastname, birthday) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthday = new Date(birthday);
+    }
+    getAge() {
+        var ageDifMs = Date.now() - this.birthday.getTime();
+        var ageDate = new Date(ageDifMs);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+}
+console.log(new ageWilmer("Wilmer", "Gonzalez", "04-07-1992").getAge());
+
--- NODEJS/Modules.js
+++ NODEJS/Modules.js
@@ -5,17 +5,17 @@
  * variables, funciones y clases definidas por otros modulos.
  * 
  */
-const stats = (function(){
-    const sum = (x,y)=> x + y;
-    const square = x => x*x;
+const stats = (function () {
+    const sum = (x, y) => x + y;
+    const square = x => x * x;
 
-    function mean(data){
-        return data.reduce(sum)/data.length;
+    function mean(data) {
+        return data.reduce(sum) / data.length;
     }
-    function stdDev(data){
+    function stdDev(data) {
         let m = mean(data);
         return Math.sqrt(
-            data.map(x=>x-m).map(square).reduce(sum)/(data.length-1)
+            data.map(x => x - m).map(square).reduce(sum) / (data.length - 1)
         );
     }
     return {
@@ -25,17 +25,17 @@ const stats = (function(){
     };
 }());
 
-console.log(stats.mean([1,3,5,7,9]));
-console.log(stats.stdDev([1,3,5,9]));
-console.log(stats.sum([1,3,5,9]));
+console.log(stats.mean([1, 3, 5, 7, 9]));
+console.log(stats.stdDev([1, 3, 5, 9]));
+console.log(stats.sum([1, 3, 5, 9]));
 
-const calculator = (function (){
-    const multiplicar = (x,y) => x*y;
-    const restar = (x,y) => x-y;
-    const dividir = (x,y) => x / y;
-    const sumar = (x,y) => x+y;
+const calculator = (function () {
+    const multiplicar = (x, y) => x * y;
+    const restar = (x, y) => x - y;
+    const dividir = (x, y) => x / y;
+    const sumar = (x, y) => x + y;
 
-    return{
+    return {
         multiplicar,
         restar,
         dividir,
@@ -44,87 +44,87 @@ const calculator = (function (){
 
 }());
 
-console.log(calculator.multiplicar(3,4));
+console.log(calculator.multiplicar(3, 4));
 
 const modules = {};
 
-function require(moduleName){
+function require(moduleName) {
     return modules[moduleName];
 }
 
-modules['statTest.js'] = (function(){
+modules['statTest.js'] = (function () {
 
     const exports = {};
-    const sum = (x,y)=> x + y;
-    const square = x => x*x;
+    const sum = (x, y) => x + y;
+    const square = x => x * x;
 
-    exports.mean = function (data){
-        return data.reduce(sum)/data.length;
+    exports.mean = function (data) {
+        return data.reduce(sum) / data.length;
     };
-    exports.stdDev = function(data){
+    exports.stdDev = function (data) {
         let m = mean(data);
         return Math.sqrt(
-            data.map(x=>x-m).map(square).reduce(sum)/(data.length-1)
+            data.map(x => x - m).map(square).reduce(sum) / (data.length - 1)
         );
     };
 
     return exports;
 }());
 
-modules['calculatorTest.js'] = (function (){
+modules['calculatorTest.js'] = (function () {
 
     const exports = {};
 
-    exports.multiplicar = function(x,y){return x *Y};
-    exports.restar = function(x,y){return x-y};
-    exports.dividir = function(x,y){return x/Y};
-    exports.sumar = function(x,y){return x+y};
+    exports.multiplicar = function (x, y) { return x * Y };
+    exports.restar = function (x, y) { return x - y };
+    exports.dividir = function (x, y) { return x / Y };
+    exports.sumar = function (x, y) { return x + y };
 
     return exports;
 
 }());
 
 const stats2 = require('statTest.js');
-console.log(stats2.mean([1,2,3,9]));
+console.log(stats2.mean([1, 2, 3, 9]));
 
 const calculator2 = require('calculatorTest.js');
-console.log(calculator2.sumar(1,4));
+console.log(calculator2.sumar(1, 4));
 
 
 /**
  * Ejercicio: Desarrollar un programa que se encargue de calcular el area de una figura dada.
  * 
  */
-modules['calcularArea.js'] = (function(){
+modules['calcularArea.js'] = (function () {
     const exports = {};
     let PI = Math.PI;
 
-    exports.area = function(x,y,z){
-        if(x.toString().toLowerCase() ==="Triangulo".toLowerCase()){
+    exports.area = function (x, y, z) {
+        if (x.toString().toLowerCase() === "Triangulo".toLowerCase()) {
             return (y * z) / 2;
         }
-        if(x.toString().toLowerCase() ==="Cuadrado".toLowerCase()){
-            return  y * y;
+        if (x.toString().toLowerCase() === "Cuadrado".toLowerCase()) {
+            return y * y;
         }
-        if(x.toString().toLowerCase() ==="Rectangulo".toLowerCase()){
-            return  y * z;
+        if (x.toString().toLowerCase() === "Rectangulo".toLowerCase()) {
+            return y * z;
         }
-        if(x.toString().toLowerCase() ==="Rombo".toLowerCase()){
-            return  (y * z) / 2;
+        if (x.toString().toLowerCase() === "Rombo".toLowerCase()) {
+            return (y * z) / 2;
         }
-            return "Debe seleccionar una figura definida";
+        return "Debe seleccionar una figura definida";
     };
-    exports.perimetro = function(x,y,z,a){
-        if(x.toString().toLowerCase() ==="Triangulo".toLowerCase()){
+    exports.perimetro = function (x, y, z, a) {
+        if (x.toString().toLowerCase() === "Triangulo".toLowerCase()) {
             return y + z + a;
         }
-        if(x.toString().toLowerCase() ==="Cuadrado".toLowerCase()){
+        if (x.toString().toLowerCase() === "Cuadrado".toLowerCase()) {
             return 4 * y;
         }
-        if(x.toString().toLowerCase() ==="Rectangulo".toLowerCase()){
-            return (2*y) + (2*z);
+        if (x.toString().toLowerCase() === "Rectangulo".toLowerCase()) {
+            return (2 * y) + (2 * z);
         }
-        if(x.toString().toLowerCase() ==="Rombo".toLowerCase()){
+        if (x.toString().toLowerCase() === "Rombo".toLowerCase()) {
             return 4 * y;
         }
 
@@ -134,15 +134,149 @@ modules['calcularArea.js'] = (function(){
 }());
 
 const stats3 = require('calcularArea.js');
-console.log("Función area: " + stats3.area("TRIANGULO",4,5));
-console.log("Función area: " + stats3.area("cuadrado",4));
+console.log("Función area: " + stats3.area("TRIANGULO", 4, 5));
+console.log("Función area: " + stats3.area("cuadrado", 4));
 console.log("Función area: " + stats3.area("trapecio"));
-console.log("Función area: " + stats3.area("recTanGUlo",45,68));
-console.log("Función area: " + stats3.area("Rombo",100,40));
-console.log("Función perimetro: " + stats3.perimetro("Trapecio",100));
-console.log("Función perimetro: " + stats3.perimetro("Triangulo",100,345,10));
-console.log("Función perimetro: " + stats3.perimetro("CUADRADO",1200));
-console.log("Función perimetro: " + stats3.perimetro("RecTanGulo",128,8272));
-console.log("Función perimetro: " + stats3.perimetro("rombos",128,8272));
-console.log("Función perimetro: " + stats3.perimetro("rombo",750));
+console.log("Función area: " + stats3.area("recTanGUlo", 45, 68));
+console.log("Función area: " + stats3.area("Rombo", 100, 40));
+console.log("Función perimetro: " + stats3.perimetro("Trapecio", 100));
+console.log("Función perimetro: " + stats3.perimetro("Triangulo", 100, 345, 10));
+console.log("Función perimetro: " + stats3.perimetro("CUADRADO", 1200));
+console.log("Función perimetro: " + stats3.perimetro("RecTanGulo", 128, 8272));
+console.log("Función perimetro: " + stats3.perimetro("rombos", 128, 8272));
+console.log("Función perimetro: " + stats3.perimetro("rombo", 750));
 
+
+/**
+ * Modulos de ECMAScript6: Los modulos en ES6 son oficiales para la mayoria de los navegadores excepto internet Explorer. En los modulos de ES6
+ * cada archivo tiene su propio contexto privado y se puede usar las declaraciones de importación y exportación. Los modulos en ES6 están en
+ * modo estricto.
+ * 
+ */
+export const PI = Math.PI;
+
+export function degreesToRadians(gradees) {
+    return (gradees * PI) / 180;
+}
+
+export class Circle {
+    constructor(radio) {
+        this.radio = radio;
+    }
+    area() {
+        return PI * this.radio * this.radio;
+    }
+}
+
+/**
+ * Segunda alternativa para exportar
+ * 
+ */
+const PI2 = Math.PI;
+
+function degreesToRadians2(gradees) {
+    return (gradees * PI) / 180;
+}
+
+class Circle2 {
+    constructor(radio) {
+        this.radio = radio;
+    }
+    area2() {
+        return PI * this.radio * this.radio;
+    }
+}
+
+
+export {PI2,degreesToRadians2,Circle2};
+
+/**
+ * En el caso de que solo queramos exportar un solo elemento utilizamos:
+ * Solo se puede hacer un default por modulo
+ */
+/**
+ * La palabra clave export solo debe aparecer en el nivel superior de nuestro codigo JavaScript, es decir, 
+ * no se puede utilizar export dentro de una clase(for,while), dentro de un ciclo, dentro de un condicional 
+ * 
+ */
+
+export default class BitSet {}
+
+/**
+ * Importt: Importa los elementos exportados de los modulos
+ */
+import BitSet from './bitset.js'; // Importando elementos con default
+
+import {mean,stdDev} from './stats.js'; // Importando varios elementos 
+
+/**
+ * Para tener en cuenta y evitar errores:
+ * 1. Las exportaciones solo pueden aparecer a nivel superior del modulo.
+ * 2. Por convención las importaciones se colocan al inicio del modulo(Buena practica de programación).
+ * 3. Para la ruta del modulo se pueden usar comillas simples o dobles ("path",'path').
+ * 4. En los modulos se pueden importar todos los elementos con una sola palabra
+ */
+import * as stats from 'stats.js'; // Importamos todos los elementos de un modulo
+
+stats.mean(); //Llama a los metodos definidos en el modulo stats
+
+/**
+ * Import para los casos especiales en los que se tenga un export default y otras exportaciones
+ * Suponiendo que el archivo bitset.js tenga default y otros elementos exportados
+ */
+import BitSet, {otroElementoExportado,otroSegundoElementoExportado} from '.bitset.js';
+
+/**
+ * Para exportar modulos que no tienen anotaciones de export
+ */
+import './archivosSinExports.js';
+
+// import renombrado: se utiliza cuando se tenga que importar dos modulos con elementos exportados que tengan nombres iguales
+
+import {mean,stdDev} from './stats.js';
+import {mean as menOther,stdDev2} from './otherstats.js';
+//renombrando elementos exportados con default
+import  {default as nuevoNombre} from './module.js';
+
+// Renombrado en los exports
+export {
+    PI2 as NumeroPI, 
+    degreesToRadians as convertir, 
+    Cicle2 as Circulo
+};
+
+/**
+ * Tener en cuenta con los exports:
+ * 1. Expresiones, por ejemplo Math.cos no están permitidas dentro del export, esto generar un 
+ * error de sintaxis.
+ */
+
+ /**
+  * Re-exports: Re-exportaciones; Implica unir dos modulos en uno solo
+  * Re-export desde nuevo archivo 'stat.js'
+  */
+import { mean } from "./stats/mean.js";
+import { stdDev } from "./stats/stdDev.js";
+
+//Re-export desde nuevo archivo 'stat.js'
+export {mean, stdDev};
+
+// Re-export desde los import
+export {mean} from './stats.js';
+export {stdDev} from '/stats/stdDev.js';
+
+// Re-export de todos los elementos desde los import
+export * from './stats.js';
+export * from '/stats/stdDev.js';
+
+/**
+ * Importar modulos re-exportados, también se pueden renombrar
+ */
+import {mean as meanOtherName, stdDev} from './stat.js';
+
+/**
+ * Re-Exportar elementos default
+ */
+export {default as mean} from './stats/mean.js';
+
+export {default as stdDev} from './stats/stdDev.js';
